d280237b0c2c4dd5a1a5df5ceab200f4ca917f0f
yangify <hongyangprivate@gmail.com>
2021-11-11 16:05:20 +0800

    updated name

--- backend/app.py
+++ backend/app.py
@@ -2,16 +2,15 @@ from flask import Flask
 from flask_cors import CORS
 
 from blueprint.auth import auth
-from blueprint.expense import expense
+from blueprint.expense import expense_blueprint
 from blueprint.project import project
 
 app = Flask(__name__)
 app.register_blueprint(auth)
-app.register_blueprint(expense)
+app.register_blueprint(expense_blueprint)
 app.register_blueprint(project)
 app.secret_key = 'supersecretkey'
 CORS(app)
-
 
 @app.route('/')
 def hello_world():
