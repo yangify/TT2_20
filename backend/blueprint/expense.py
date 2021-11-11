17e4ddebf05be9451175f136eb2cad5efd0f19ec
yangify <hongyangprivate@gmail.com>
2021-11-11 16:34:38 +0800

    added user_id parameter

--- backend/blueprint/expense.py
+++ backend/blueprint/expense.py
@@ -2,19 +2,16 @@ import json
 
 from flask import Blueprint, session, request
 from datetime import datetime
-from blueprint.db import Database
+from backend.blueprint.db import Database
 
 expense_blueprint = Blueprint('expense', __name__)
 db = Database()
 
 
-@expense_blueprint.route('/expenses/<project_id>', methods=['GET'])
-@expense_blueprint.route('/expenses/<project_id>/<expense_id>', methods=['GET'])
-def get_expenses(project_id, expense_id=None):
-    if 'user' not in session or not session['user']:
-        return {'status': False, 'message': 'no user logged in'}
-
-    if not has_project(project_id):
+@expense_blueprint.route('/expenses/<project_id>/<user_id>', methods=['GET'])
+@expense_blueprint.route('/expenses/<project_id>/<user_id>/<expense_id>', methods=['GET'])
+def get_expenses(project_id, user_id, expense_id=None):
+    if not has_project(project_id, user_id):
         return {'status': False, 'message': 'project not found'}
 
     expenses = [e for e in db.expenses if e['project_id'] == int(project_id)]
@@ -27,9 +24,8 @@ def get_expenses(project_id, expense_id=None):
     return {'status': True, 'expense': output}
 
 
-@expense_blueprint.route('/expenses', methods=['POST'])
-def create_expense():
-    if 'user' not in session or not session['user']: return {'status': False, 'message': 'access denied'}
+@expense_blueprint.route('/expenses/<user_id>', methods=['POST'])
+def create_expense(user_id):
 
     project_id = request.form['project_id']
     expense_name = request.form['expense_name']
@@ -37,7 +33,7 @@ def create_expense():
     expense_description = request.form['expense_description']
     amount = request.form['amount']
 
-    if not has_project(project_id):
+    if not has_project(project_id, user_id):
         return {'status': False, 'message': 'project not found'}
     check_exist = None
     for e in db.expenses:
@@ -64,13 +60,13 @@ def create_expense():
     return {'status': True, 'message': 'new expense created'}
 
 
-@expense_blueprint.route('/expenses', methods=['PUT'])
-def update_expenses():
+@expense_blueprint.route('/expenses/<user_id>', methods=['PUT'])
+def update_expenses(user_id):
     new_expense = json.loads(request.data)
 
     expense = get_expense(new_expense['id'])
     if not expense: return {'status': False, 'message': 'expense not found'}
-    if not has_project(expense['project_id']): return {'status': False, 'message': 'access denied'}
+    if not has_project(expense['project_id'], user_id): return {'status': False, 'message': 'access denied'}
 
     for key, value in new_expense.items():
         if key not in expense: continue
@@ -80,16 +76,14 @@ def update_expenses():
 
 
 # delete expense in project
-@expense_blueprint.route('/expenses/<expense_id>', methods=['DELETE'])
-def delete_expense(expense_id):
-    if 'user' not in session or not session['user']: return {'status': False, 'message': 'access denied'}
-
+@expense_blueprint.route('/expenses/<expense_id>/<user_id>', methods=['DELETE'])
+def delete_expense(expense_id, user_id):
     exp = None
     for e in db.expenses:
         if e['id'] == int(expense_id): exp = e
     if not exp: return {'status': False, 'message': 'expense not found'}
 
-    if not has_project(exp['project_id']): return {'status': False, 'message': 'access denied'}
+    if not has_project(exp['project_id'], user_id): return {'status': False, 'message': 'access denied'}
     db.expenses.remove(exp)
     return {'status': True, 'expense': exp}
 
@@ -100,14 +94,12 @@ def get_expense(expense_id):
     return None
 
 
-def has_expense(expense):
-    user_id = session['user']['id']
+def has_expense(expense, user_id):
     project = get_project(expense['project_id'])
     return project['user_id'] == user_id and expense['project_id'] == project['id']
 
 
-def has_project(project_id):
-    user_id = session['user']['id']
+def has_project(project_id, user_id):
     for p in db.projects:
         if p['id'] == int(project_id) and p['user_id'] == user_id:
             return True
