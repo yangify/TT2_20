8c332d1ff7a93d6b07466ecb04a1c22373ff2c17
Weilun <48602497+weilun96@users.noreply.github.com>
2021-11-11 15:18:08 +0800

    projects route

--- frontend/tt2_20/src/App.js
+++ frontend/tt2_20/src/App.js
@@ -1,18 +1,18 @@
 import "./App.css";
-import { Routes , Route, HashRouter} from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import Login from "./components/login";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import ProjectTable from "./components/ProjectTable";
 
 function App() {
   return (
     <div className="App">
-    <HashRouter>
-      <Routes>
-        <Route exact path="/" element={<Login/>}/>
-      </Routes>
-    </HashRouter>
-   
-     
+      <HashRouter>
+        <Routes>
+          <Route path="/projects" element={<ProjectTable />} />
+          <Route exact path="/" element={<Login />} />
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
