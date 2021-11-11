522fdc1f2a6b41a2edea2789f95311ff5fcc22b5
Xiaocow <lonleqwert92@hotmail.com>
2021-11-11 12:16:03 +0800

    Fixed Routers

--- frontend/tt2_20/src/App.js
+++ frontend/tt2_20/src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes , Route, HashRouter} from "react-router-dom";
 import Home from "./components/Home"
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route exact path="/" exact component={Home} />
-        </Routes>
-      </Router>
+    <HashRouter>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+      </Routes>
+    </HashRouter>
+   
+     
     </div>
   );
 }
