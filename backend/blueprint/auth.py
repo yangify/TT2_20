42b09b8d9d8e5f3d4450c543085753e7510b4125
tzh-zachary <tanzh19@gmail.com>
2021-11-11 11:44:15 +0800

    basic user auth

--- backend/blueprint/auth.py
+++ backend/blueprint/auth.py
@@ -1,11 +1,32 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request
+import json
+
+from db import Database
 
 auth = Blueprint('login', __name__)
 
-
+database = Database()
 @auth.route('/login')
-def hello_world():
-    return 'login!'
+def login():
+    jsdata = request.get_json()
+    output = authenticate(jsdata)
+    output = json.dumps(output, default=str)
+    return jsonify({"body": output})
+  
+def authenticate(logindata):
+    getusers = database.users()
+    for user in getusers:
+        if logindata['username'] == user['username'] and logindata['password'] == user['password']:
+            output = {
+                "logged": True,
+                "user": user["username"]
+            }
+            return output
+    output = {
+        "logged": False,
+        "output": "Username/Password is wrong"
+    }
+    return output
 
 
 @auth.route('/logout')
