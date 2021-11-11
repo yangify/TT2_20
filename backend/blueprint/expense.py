ff83dc72725a23acfb44f51f62aa6bd85fa10ff3
yangify <hongyangprivate@gmail.com>
2021-11-11 15:27:22 +0800

    added update endpoint

--- backend/blueprint/expense.py
+++ backend/blueprint/expense.py
@@ -1,12 +1,14 @@
+import json
+
 from flask import Blueprint, session, request
 from blueprint.db import Database
 
-expense = Blueprint('expense', __name__)
+expense_blueprint = Blueprint('expense', __name__)
 db = Database()
 
 
-@expense.route('/expenses/<project_id>', methods=['GET'])
-@expense.route('/expenses/<project_id>/<expense_id>', methods=['GET'])
+@expense_blueprint.route('/expenses/<project_id>', methods=['GET'])
+@expense_blueprint.route('/expenses/<project_id>/<expense_id>', methods=['GET'])
 def get_expenses(project_id, expense_id=None):
     if 'user' not in session or not session['user']:
         return {'status': False, 'message': 'no user logged in'}
@@ -24,16 +26,23 @@ def get_expenses(project_id, expense_id=None):
     return {'status': True, 'expense': output}
 
 
-def has_project(project_id):
-    user_id = session['user']['id']
-    for p in db.projects:
-        if p['id'] == int(project_id) and p['user_id'] == user_id:
-            return True
-    return False
+@expense_blueprint.route('/expenses', methods=['PUT'])
+def update_expenses():
+    new_expense = json.loads(request.data)
+
+    expense = get_expense(new_expense['id'])
+    if not expense: return {'status': False, 'message': 'expense not found'}
+    if not has_project(expense['project_id']): return {'status': False, 'message': 'access denied'}
+
+    for key, value in new_expense.items():
+        if key not in expense: continue
+        expense[key] = value
+
+    return {'status': True, 'expense': expense}
 
 
 # delete expense in project
-@expense.route('/expenses/<expense_id>', methods=['DELETE'])
+@expense_blueprint.route('/expenses/<expense_id>', methods=['DELETE'])
 def delete_expense(expense_id):
     if 'user' not in session or not session['user']: return {'status': False, 'message': 'access denied'}
 
@@ -45,3 +54,29 @@ def delete_expense(expense_id):
     if not has_project(exp['project_id']): return {'status': False, 'message': 'access denied'}
     db.expenses.remove(exp)
     return {'status': True, 'expense': exp}
+
+
+def get_expense(expense_id):
+    for expense in db.expenses:
+        if expense['id'] == expense_id: return expense
+    return None
+
+
+def has_expense(expense):
+    user_id = session['user']['id']
+    project = get_project(expense['project_id'])
+    return project['user_id'] == user_id and expense['project_id'] == project['id']
+
+
+def has_project(project_id):
+    user_id = session['user']['id']
+    for p in db.projects:
+        if p['id'] == int(project_id) and p['user_id'] == user_id:
+            return True
+    return False
+
+
+def get_project(project_id):
+    for p in db.projects:
+        if p['id'] == project_id: return p
+    return None
