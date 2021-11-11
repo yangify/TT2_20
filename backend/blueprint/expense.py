a983f85d7cd51a5fdee15b6b837c79a10530dee6
yangify <hongyangprivate@gmail.com>
2021-11-11 12:21:10 +0800

    updated expense structure

--- backend/blueprint/expense.py
+++ backend/blueprint/expense.py
@@ -1,8 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, session
+
+from backend.blueprint.db import Database
 
 expense = Blueprint('expense', __name__)
+db = Database()
 
 
-@expense.route('/expense')
-def hello_world():
-    return 'Hello expense!'
+@expense.route('/expense', methods=['GET'])
+def get_expense():
+    return session['user']
