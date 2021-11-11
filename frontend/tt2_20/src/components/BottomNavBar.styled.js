9db6fd1f7950461492a2e6dfa23eb4d525d24822
Xiaocow <lonleqwert92@hotmail.com>
2021-11-11 13:43:47 +0800

    Navigation Bar done. Top and Bottom

---
+++ frontend/tt2_20/src/components/BottomNavBar.styled.js
@@ -0,0 +1,45 @@
+
+import styled from "styled-components";
+import { FaBars } from "react-icons/fa";
+
+export const Nav = styled.nav`
+    background: #4341A1;
+    height: 55px;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0.2rem calc((100vw - 1000px) / 2);
+    z-index: 12;
+`;
+
+
+export const Bars = styled(FaBars)`
+  display: none;
+  color: #fff;
+  @media screen and (max-width: 768px) {
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100%, 75%);
+    font-size: 1.8rem;
+    cursor: pointer;
+  }
+`;
+
+export const NavMenu = styled.div`
+ 
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+export const NavBtn = styled.nav`
+  display: flex;
+  align-items: center;
+  margin-right: 24px;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
--- frontend/tt2_20/src/components/login.js
+++ frontend/tt2_20/src/components/login.js
@@ -3,7 +3,8 @@
     import FormControl from '@mui/material/FormControl';
     import TextField from '@material-ui/core/TextField'; 
     import { render } from "react-dom";
-
+    import TopNavBar from "./TopNavBar";
+    import BottomNavBar from "./BottomNavBar";
 class login extends React.Component {
     constructor(props){
         super(props);
@@ -11,13 +12,15 @@ class login extends React.Component {
 
     render(){
         return (
+            
             <div>
-                <h1>Home Page</h1>
+                <TopNavBar/>
                 <form className={FormControl}>
                     <TextField id="username" label="Username:" variant="standard" />
                     <TextField id="password" label="Password" variant="standard" />
                     <input type="submit" onSubmit></input>
                 </form>
+                <BottomNavBar/>
             </div>
             
         );
